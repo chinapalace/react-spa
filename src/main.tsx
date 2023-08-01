70e2b17972608a5080481708359780449b2b1108
chinapalace <justinmclaren63@gmail.com>
2023-08-01 13:12:38 -0700

    feature(webbridge-sdk): update to 0.0.7

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from 'App'
-import { StrictMode } from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import packageData from '../package.json'
@@ -19,6 +20,9 @@ const queryClient = new QueryClient({
 		}
 	}
 })
+
+const indexReact = React
+console.log(indexReact)
 
 const container = document.querySelector('#root')
 if (container) {
