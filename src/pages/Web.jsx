d8bf048823ddb52399df51941621748cca881471
chinapalace <justinmclaren63@gmail.com>
2023-11-02 13:15:50 -0700

    fix(variables): stringify

--- src/pages/Web.jsx
+++ src/pages/Web.jsx
@@ -86,7 +86,7 @@ const TapcartBlock = () => {
 		<div className='mx-auto max-w-xl px-5 font-sans text-sm text-white'>
 			<h2 className='py-2 text-white'>Variables</h2>
 			<div className='mb-2 rounded bg-gray-800 p-5'>
-				<pre>{variables}</pre>
+				<pre>{JSON.stringify(variables, null, 2)}</pre>
 			</div>
 
 			<h2 className='py-2 text-white'>Discount Inputs & Actions</h2>
